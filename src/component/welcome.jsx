860b1f07cbae07affd9e6188f108289dce97df74
Nitinkumar Yadav <126796871+Nitinkumar-yadav@users.noreply.github.com>
2024-02-29 18:41:05 +0000

    Initial commit

---
+++ src/component/welcome.jsx
@@ -0,0 +1,11 @@
+import GameBoard from "./gameBoard"
+
+
+export default function Welcome() {
+    return(
+        <div>
+            <h1>Main Screen</h1>
+            <GameBoard/>
+        </div>
+    )
+}
