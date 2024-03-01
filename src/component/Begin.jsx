7417e27e9dea1d6e8bbac53c7e7d10c0897b7fd7
Nitinkumar Yadav <126796871+Nitinkumar-yadav@users.noreply.github.com>
2024-03-01 10:56:20 +0000

    update commites

--- src/component/Begin.jsx
+++ src/component/Begin.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
-import '../component/min.css';
+// import '../component/min.css';
 import Image from '../image/open-month-monkey.svg';
-import Instruction from './Instructions';
 
-export default function Welcome() {
+export default function Begin() {
     const[start,setStart] =useState(false);
 
     return(
         <div className="main-screen">
-
-                 <h1><span>Welcome Kiddo !</span></h1>
+           {/* <button></button> */}
+                 <h1><span>Can you help me get some ? </span></h1>
             <div className="sub-screen">
                 <img src={Image} alt="Monkey"/>
             </div>
-            <button onClick={start ? () => setStart(false) : () => setStart(true)}/>
-            {start && <Instruction />}
+            <button onClick={start ? () => setStart(false) : () => setStart(true)}>
+  {/* {start && <Begin/>} */}
+</button>
         </div>
     )
 }
--- src/component/Instructions.jsx
+++ src/component/Instructions.jsx
@@ -1,26 +1,22 @@
-
-
 import { useState } from 'react';
-
-import '../component/min.css';
+// import '../component/min.css';
 import Image from '../image/open-month-monkey.svg';
-import GameBoard from './gameBoard';
-import Instruction from './Instructions';
+import Begin from './Begin';
+
 
 export default function Instructions() {
     const[start,setStart] =useState(false);
 
     return(
         <div className="main-screen">
-           <button>{background}</button>
                  <h1><span>Hi , I am Mizo !
 and I love bananas</span></h1>
             <div className="sub-screen">
                 <img src={Image} alt="Monkey"/>
             </div>
-            {/* <button onClick={start ? () => setStart(false) : () => setStart(true)}>
-  {start && <Instruction/>} */}
-{/* </button> */}
+            <button onClick={start ? () => setStart(false) : () => setStart(true)}>
+  {start && <Begin/>}
+</button>
         </div>
     )
 }
