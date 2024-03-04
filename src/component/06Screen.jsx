6328916743582ec4712079a3165b77c10a76d1e0
Nitin <nitindv260@gmail.com>
2024-03-05 00:43:13 +0530

    final

--- src/component/06Screen.jsx
+++ src/component/06Screen.jsx
@@ -5,13 +5,13 @@ import Bg from "../image/02-sec-screen.svg";
 const GameOver = ({ restartGame }) => {
   return (
     <div className="main-screen" style={{ backgroundImage: `url(${Bg})` }}>
-    <div className="centered">
+    {/* <div className="centered"> */}
       <h1>Congrats!</h1>
       <button className="restart-button" onClick={restartGame}>
         Play Again?
       </button>
     </div>
-    </div>
+    // </div>
   );
 };
 
