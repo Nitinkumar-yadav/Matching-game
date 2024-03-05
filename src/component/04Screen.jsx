b184591023a3ba1531d45c01a1ab5f86d11a3407
Nitin <nitindv260@gmail.com>
2024-03-05 08:52:46 +0530

    commit

--- src/component/04Screen.jsx
+++ src/component/04Screen.jsx
@@ -7,7 +7,7 @@ import blueCard from '../image/blue-card.svg';
 import cardbg from '../image/card-bg.svg';
 import twiceCard from '../image/twice-card.svg';
 import redCard2 from '../image/red-card2.svg';
-import BananaLoader from '../image/loader-banana.svg';
+
 import './CSS/min.css';
 
 export default function Playgame({ currentScreen, onNext, onBack }) {
@@ -22,26 +22,24 @@ export default function Playgame({ currentScreen, onNext, onBack }) {
 
   return (
     <div className="main-screen" style={{ backgroundImage: `url(${Bg})` }}>
-      <img className="allow-icon" src={Allow} alt="Back" onClick={handleBack} style={{ display: currentScreen === 'Playgame' }} />
-      <img className="loader" style={{ margin: '10px 10px 10px 100px', height: '5vh' }} src={Loader} alt="Loader" />
-      {/* <img className="banana-loader" style={{height:'15vh'}} src={BananaLoader} alt="Banana"/> */}
+      <img 
+        className="allow-icon" 
+        src={Allow} 
+        alt="Back" 
+        onClick={handleBack} 
+        style={{ display: currentScreen === 'Playgame' }} 
+      />
+      <img className="loader" src={Loader} alt="Loader" />
       <div className="sub-screen">
-        <div style={{ position: 'absolute', right: '10%', display: 'flex', flexDirection: 'row', gap: '10%', justifyContent: 'center' }}>
-          <div className='card-img' style={{ width: '16rem', height: '20rem', backgroundImage: `url(${cardbg})` }}>
+        <div className='card-container'>
+          <div className='card-img' style={{backgroundImage: `url(${cardbg})` }}>
             <img style={{ width: '15rem' }} src={redCard2} />
-              {/* <h6>Select a pink card.</h6>
-              <p>It has images.</p> */}
           </div>
-          <div className='card-img' style={{ width: '16rem', height: '20rem', backgroundImage: `url(${cardbg})` }}>
+          <div className='card-img' style={{backgroundImage: `url(${cardbg})` }}>
             <img style={{ width: '15rem' }} src={blueCard} />
-              {/* <h6>Select a pink card.</h6>
-              <p>It has alphabets.</p> */}
           </div>
-          <div className='card-img' style={{ width: '16rem', height: '20rem', backgroundImage: `url(${cardbg})` }}>
+          <div className='card-img' style={{backgroundImage: `url(${cardbg})` }}>
             <img style={{ width: '15rem' }} src={twiceCard} />
-              {/* <p>If they're same</p>
-              <h6>Its a match !</h6>
-              <p>otherwise retry!</p> */}
           </div>
         </div>
       </div>
